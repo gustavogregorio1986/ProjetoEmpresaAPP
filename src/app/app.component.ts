5ef1836df91ed7808bb760fd560188684d8a1e7a
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-07-17 14:43:53 -0300

    desenvolvimento da pagina inical da home no projeto com angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'projeto-empresa';
+  title = 'projeto de desenvolvimento web - empresa';
+
+  empresa = 'https://controle-rs.com.br/wp-content/uploads/2022/08/Como-definir-os-valores-de-sua-empresa-Blog.png'
+  altEmpresa = 'ilustração da empresa'
 }
